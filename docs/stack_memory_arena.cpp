94fcba803417d5e4b30d7f81e332402d08fdf2f9
Letheward <letheward@foxmail.com>
2023-04-18 22:28:38 +0800

    cleanup

--- docs/stack_memory_arena.cpp
+++ docs/stack_memory_arena.cpp
@@ -9,14 +9,13 @@ int main() {
     auto arena       = memory_arena(main_arena_data);
     auto table_arena = arena.eat_from_top(1024 * 32);
     
-    Allocator string_allocator = { arena_allocator_procedure, &arena };
-    Allocator table_allocator  = { arena_allocator_procedure, &table_arena };
+    Allocator temp = { arena_allocator_procedure, &arena };
 
     srand((u32) time(0));
 
     auto get_random_string = [&]() -> String {
         u64 count = 5 + rand() % 8;
-        auto [out, _] = String::alloc(count, string_allocator);
+        auto [out, _] = String::alloc(count, temp);
         for (u64 i = 0; i < out.count; i++) {
             out.data[i] = (u8) (rand() % ('z' - 'a') + 'a');
         }
@@ -24,14 +23,14 @@ int main() {
     };
     
     HashTable<String, String> table;
-    table.init(16, table_allocator);
+    table.init(16, { arena_allocator_procedure, &table_arena });
 
     for (u64 i = 0; i < 20; i++) {
         table.add(get_random_string(), get_random_string());
     }
 
     StringBuilder builder;
-    builder.init(1024, string_allocator);
+    builder.init(1024, temp);
 
     table.for_all([&](auto entry, auto index) {
         builder.print("%.4llu [%.8x] ", index, entry->hash);
